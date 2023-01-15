3669e6b1cc7a025bf116bb4c5983c04d16ca3aaf
Amber Kimbrough <akimbrough14@gmail.com>
2023-01-15 12:39:23 -0600

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    return cats.push(`${name}`)
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(`${name}`)
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift()
+}
+
+function appendCat(name) {
+    const copyCats = [...cats, `${name}`]
+    return copyCats
+
+}
+
+function prependCat(name) {
+    const copyCats = [`${name}`, ...cats]
+    return copyCats
+}
+
+function removeLastCat() {
+    const copyCats = [...cats]
+    copyCats.pop()
+    return copyCats
+}
+
+function removeFirstCat() {
+    const copyCats = [...cats]
+    copyCats.shift()
+    return copyCats
+}
